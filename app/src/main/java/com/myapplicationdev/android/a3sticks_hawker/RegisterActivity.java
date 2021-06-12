b0a308ee19e21a668c36fb8f2d42033feb7e4f61
ys2409 <19022397@myrp.edu.sg>
2021-06-12 20:00:56 +0800

    ys commit

--- app/src/main/java/com/myapplicationdev/android/a3sticks_hawker/RegisterActivity.java
+++ app/src/main/java/com/myapplicationdev/android/a3sticks_hawker/RegisterActivity.java
@@ -3,6 +3,9 @@ package com.myapplicationdev.android.a3sticks_hawker;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import com.loopj.android.http.*;
+import cz.msebera.android.httpclient.*;
+
 
 public class RegisterActivity extends AppCompatActivity {
 
--- app/src/main/java/com/myapplicationdev/android/a3sticks_hawker/TimeActivity.java
+++ app/src/main/java/com/myapplicationdev/android/a3sticks_hawker/TimeActivity.java
@@ -1,6 +1,9 @@
 package com.myapplicationdev.android.a3sticks_hawker;
 
 import androidx.appcompat.app.AppCompatActivity;
+import com.loopj.android.http.*;
+import cz.msebera.android.httpclient.*;
+
 
 public class TimeActivity extends AppCompatActivity {
 
