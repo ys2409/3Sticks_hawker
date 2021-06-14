e3e370ee6aaaec632953a86eba3b56d16ba53ecf
ys2409 <19022397@myrp.edu.sg>
2021-06-14 15:04:40 +0800

    ys commit

--- app/src/main/java/com/myapplicationdev/android/a3sticks_hawker/ProfileFragment.java
+++ app/src/main/java/com/myapplicationdev/android/a3sticks_hawker/ProfileFragment.java
@@ -8,7 +8,19 @@ import androidx.fragment.app.Fragment;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ArrayAdapter;
 import android.widget.TextView;
+
+import com.loopj.android.http.AsyncHttpClient;
+import com.loopj.android.http.JsonHttpResponseHandler;
+
+import org.json.JSONArray;
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+
+import cz.msebera.android.httpclient.Header;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -64,12 +76,34 @@ public class ProfileFragment extends Fragment {
     @Override
     public View onCreateView(LayoutInflater inflater, ViewGroup container,
                              Bundle savedInstanceState) {
+
+        ArrayList<Profile> alProfile = new ArrayList<Profile>();
+        ArrayAdapter<Profile> aaProfile = null;
+        
         // Inflate the layout for this fragment
         View view = inflater.inflate(R.layout.fragment_profile, container, false);
 
         toolbar = view.findViewById(R.id.top_toolbar);
         TextView tb = view.findViewById(R.id.toolbar_title);
         tb.setText("Orders");
+
+        AsyncHttpClient client = new AsyncHttpClient();
+        client.get("http://10.0.2.2/c302_sakila/getProfile.php", new JsonHttpResponseHandler(){
+            @Override
+            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
+                //called when response HTTP status is "200 OK"
+                try {
+                    for(int i = 0; i<response.length(); i++){
+                        JSONObject profile = (JSONObject)response.get(i);
+                        Profile p = new Profile(profile.getInt("owner_id"), profile.getString("name"));
+                        alProfile.add(p);
+                    }
+                } catch(JSONException e){
+
+                }
+                aaProfile.notifyDataSetChanged();
+            }
+        });
 
         return view;
     }
--- app/src/main/java/com/myapplicationdev/android/a3sticks_hawker/RegisterActivity.java
+++ app/src/main/java/com/myapplicationdev/android/a3sticks_hawker/RegisterActivity.java
@@ -18,31 +18,12 @@ import cz.msebera.android.httpclient.*;
 
 public class RegisterActivity extends AppCompatActivity {
 
-    ArrayList<Profile> alProfile = new ArrayList<Profile>();
-    ArrayAdapter<Profile> aaProfile;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
 
-        AsyncHttpClient client = new AsyncHttpClient();
-        client.get("http://10.0.2.2/c302_sakila/getProfile.php", new JsonHttpResponseHandler(){
-            @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
-                //called when response HTTP status is "200 OK"
-                try {
-                    for(int i = 0; i<response.length(); i++){
-                        JSONObject profile = (JSONObject)response.get(i);
-                        Profile p = new Profile(profile.getInt("owner_id"), profile.getString("name"));
-                        alProfile.add(p);
-                    }
-                } catch(JSONException e){
-
-                }
-                aaProfile.notifyDataSetChanged();
-            }
-        });
     }
 
 
