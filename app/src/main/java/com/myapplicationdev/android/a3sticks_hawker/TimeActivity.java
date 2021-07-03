f7f9965b87fa6723b6078b9592a0e0328052cc21
19022187 <19022187@myrp.edu.sg>
2021-07-03 11:48:42 +0800

    ...

--- app/src/main/java/com/myapplicationdev/android/a3sticks_hawker/TimeActivity.java
+++ app/src/main/java/com/myapplicationdev/android/a3sticks_hawker/TimeActivity.java
@@ -1,6 +1,8 @@
 package com.myapplicationdev.android.a3sticks_hawker;
 
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 import androidx.appcompat.app.AppCompatActivity;
 import com.loopj.android.http.*;
@@ -9,10 +11,19 @@ import cz.msebera.android.httpclient.*;
 
 public class TimeActivity extends AppCompatActivity {
 
+    Button btnSave;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_set_time);
 
+        btnSave = findViewById(R.id.btnSaveTime);
+        btnSave.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+
+            }
+        });
     }
 }
