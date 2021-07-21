0a02cea09b761075e0d1fe4cfe587e50a2ce401f
ys2409 <19022397@myrp.edu.sg>
2021-07-21 22:26:26 +0800

    ys commit

--- app/src/main/java/com/myapplicationdev/android/a3sticks_hawker/ChangePassword.java
+++ app/src/main/java/com/myapplicationdev/android/a3sticks_hawker/ChangePassword.java
@@ -1,8 +1,14 @@
 package com.myapplicationdev.android.a3sticks_hawker;
 
+import android.content.SharedPreferences;
 import android.os.Bundle;
+import android.preference.PreferenceManager;
+import android.util.Log;
+import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -18,32 +24,46 @@ import cz.msebera.android.httpclient.Header;
 
 public class ChangePassword extends AppCompatActivity {
     TextView tvCurrPass1;
+    private AsyncHttpClient client;
+    EditText etPassword;
+    Button btnConfirm;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_change_password);
 
-        tvCurrPass1 = findViewById(R.id.tvCurrPass1);
+        client = new AsyncHttpClient();
 
-        RequestParams params = new RequestParams();
+        etPassword = findViewById(R.id.etPassword);
+        btnConfirm = findViewById(R.id.button);
 
-        AsyncHttpClient client = new AsyncHttpClient();
-        client.get("http://10.0.2.2/3Sticks_hawker/3Sticks_hawker/getProfile.php", new JsonHttpResponseHandler(){
+        SharedPreferences prefs = PreferenceManager.getDefaultSharedPreferences(ChangePassword.this);
+        String ownerID = prefs.getString("ownerID", "");
+
+        btnConfirm.setOnClickListener(new View.OnClickListener() {
             @Override
-            public void onSuccess(int statusCode, Header[] headers, JSONArray response) {
-                //called when response HTTP status is "200 OK"
-                try {
-                    for(int i = 0; i<response.length(); i++){
-                        JSONObject profile = (JSONObject)response.get(i);
-                        String p = profile.getString("password");
-                        tvCurrPass1.setText(p.toString());
-                        //alProfile.add(p);
-                    }
-                } catch(JSONException e){
+            public void onClick(View view) {
+                RequestParams params = new RequestParams();
+                params.add("ownerID", ownerID);
+                params.add("newPass", etPassword.getText().toString());
+                Log.d("TAG", params.toString());
 
-                }
-                //aaProfile.notifyDataSetChanged();
+                client.post("http://10.0.2.2/3Sticks_hawker/3Sticks_hawker/editPassword.php", params, new JsonHttpResponseHandler(){
+                    @Override
+                    public void onSuccess(int statusCode, Header[] headers, JSONObject response) {
+                        //called when response HTTP status is "200 OK"
+                        try {
+                            Toast.makeText(getApplicationContext(), "Password successfully updated", Toast.LENGTH_SHORT).show();
+                            finish();
+                        } catch(Exception e){
+                            e.printStackTrace();
+                        }
+                        //aaProfile.notifyDataSetChanged();
+                    }
+
+                });
+                Log.d("TAG", "onCreate: ");
             }
         });
     }
